770a7d6e7b57e0351d89d99b3a68ecd6cdc3043a
이현빈 <67374247+991357@users.noreply.github.com>
2025-08-24 15:14:08 +0900

    Create [BOJ] 스타트링크_5014_실버1.java

---
+++ 이현빈/이현빈[BFS, DFS]/[BOJ] 스타트링크_5014_실버1.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int F, S, G, U, D;
+
+    static Deque<Integer> bfsQ;
+    static int[] countArr;
+
+    public static void main(String[] args) throws Exception
+    {
+        st = new StringTokenizer(br.readLine());
+        F = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        G = Integer.parseInt(st.nextToken());
+        U = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+
+        countArr = new int[F + 1];
+        Arrays.fill(countArr, -1);
+
+        bfs();
+        System.out.println(countArr[G] == -1 ? "use the stairs" : countArr[G]);
+    }
+
+    private static void bfs()
+    {
+        bfsQ = new ArrayDeque<>();
+        bfsQ.offer(S);
+        countArr[S] = 0;
+
+        while (!bfsQ.isEmpty())
+        {
+            int cur = bfsQ.poll();
+            S = cur;
+
+            if (cur == G)
+                return;
+
+            // 위
+            if (U != 0 && cur + U <= F && countArr[cur + U] == -1)
+            {
+                bfsQ.offer(cur + U);
+                countArr[cur + U] = 1 + countArr[cur];
+            }
+
+            // 아래
+            if (D != 0 && cur - D >= 1 && countArr[cur - D] == -1)
+            {
+                bfsQ.offer(cur - D);
+                countArr[cur - D] = 1 + countArr[cur];
+            }
+        }
+    }
+}
