0adc1a52e1f4644bffa7cfef62c445f87b470d3b
0gi-h <103099496+0gi-h@users.noreply.github.com>
2025-09-01 07:40:33 +0900

    Update [BOJ] 캐슬 디펜스_17135_골드3.java

--- 황기영/황기영[A형]/[BOJ] 캐슬 디펜스_17135_골드3.java
+++ 황기영/황기영[A형]/[BOJ] 캐슬 디펜스_17135_골드3.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
  
-public class Main {
+public class Q17135 {
    static int N,M,D;
    static int[] picked = new int[3];
    static int[][] map;
@@ -13,7 +13,7 @@ public class Main {
       M = Integer.parseInt(st.nextToken());
       D = Integer.parseInt(st.nextToken());
       map = new int[N][M];
-	  for(int i=0;i<N;i++) {
+      for(int i=0;i<N;i++) {
          st = new StringTokenizer(br.readLine());
          for(int j=0;j<M;j++){
             map[i][j] = Integer.parseInt(st.nextToken());
@@ -40,6 +40,10 @@ public class Main {
       pick(idx, start + 1);
    }
  
+	 //상태를 저장할 배열 status
+	 //궁수를 한 줄씩 올리는 방식(line)
+	 //거리 1~D까지 반복문으로 돌면서 attack 호출
+	 //잡으면 반환값을 count에 추가
    public static int game(){
       int count = 0;
       int[][] status = new int[N][M];
@@ -58,6 +62,10 @@ public class Main {
       return count;
    }
  
+	 //pick은 궁수가 배치된 열, +- d의 배열만 탐색
+	 //적의 위치(nn, nm), nn은 d-(Math.abs(nm - pick), 여기가 핵심
+	 //status배열이 0이면 아직 처치하지 않은 적, 현재 궁수의 행(line)을 저장
+	 //현재 line과 status배열의 값이 같다면 다른 궁수가 쏜 적
    public static int attack(int[][] status, int d, int line, int pick){
       int nn;
       for(int nm=pick-d;nm<=pick+d;nm++){
